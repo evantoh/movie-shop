2671a5786135967d528756460b9babee4a5640d0
evantoh <evanmwenda@gmail.com>
2018-01-22 17:39:12 +0300

    finish monday work

---
+++ app/config.py
@@ -0,0 +1,13 @@
+class Config:
+
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    
+    # pass
+
+class ProdConfig(Config):
+
+    pass
+
+class DevConfig(Config):
+
+    DEBUG = True
