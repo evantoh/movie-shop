f6c62a8d0f4478ea459e18dc4c888ef0946213ef
evantoh <evanmwenda@gmail.com>
2018-01-23 11:56:12 +0300

    display image and movie details

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+# from app import views
 from .config import DevConfig
 
 # Initializing application
-app = Flask(__name__,instance_relative_config = True)
+app = Flask(__name__, instance_relative_config = True)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
 bootstrap = Bootstrap(app)
+
+
 
 from app import views
